901e13f60e56f7b242c6095c025efef15758afb4
kaleel <pixelhubster@gmail.com>
2025-05-01 08:20:12 +0000

    added env to production

--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -6,13 +6,11 @@ const Navbar = dynamic(() => import('@/components/navbar'), { ssr: false })
 export default function Home() {
    return (
       <main className="w-full h-screen bg-[#F7F7FF] m-0 p-0">
-         {/* <Suspense> */}
-            <Navbar />
-         {/* </Suspense> */}
-            <div className="w-full flex flex-shrink" style={{ height: 'calc(100% - 5rem)' }}>
-               <Leftpanel />
-               <Rightpanel />
-            </div>
+         <Navbar />
+         <div className="w-full flex flex-shrink" style={{ height: 'calc(100% - 5rem)' }}>
+            <Leftpanel />
+            <Rightpanel />
+         </div>
       </main>
    );
 }
